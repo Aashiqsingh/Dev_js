2fcf41e41917511da9ce130c8b251fa5921aa2fb
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-08-30 10:38:19 +0530

    mark

---
+++ Basic/Marks.js
@@ -0,0 +1,12 @@
+var eng = 90;
+var comp = 90;
+var math = 90;
+
+
+var total = eng + comp + math;
+
+console.log("Total marks of three subjects are = ",total);
+
+var per = total/3;
+console.log("Percentage = ",per);
+
