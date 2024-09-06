b234d964d1b8e1ad0e2244fe08c11d25bae551fc
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-09-06 12:31:44 +0530

    var , let , const

--- DataType/demo1.js
+++ DataType/demo1.js
@@ -1,25 +1,28 @@
 // Datatype : number , string , boolean , undefined
 
 
-a = 10;
-console.log("value of a = ",a);
-console.log("type of a = ",typeof(a));
+// a = 10;
+// console.log("value of a = ",a);
+// console.log("type of a = ",typeof(a));
 
-b = 13.5;
-console.log("value of b = ",b);
-console.log("type of b = ",typeof(b));
+// b = 13.5;
+// console.log("value of b = ",b);
+// console.log("type of b = ",typeof(b));
 
-c = "dev nagariya jii";
-console.log("value of c = ",c);
-console.log("type of c = ",typeof(c));
+// c = "dev nagariya jii";
+// console.log("value of c = ",c);
+// console.log("type of c = ",typeof(c));
 
 
-var ismarried = false;
-console.log("value of ismarried = ",ismarried);
-console.log("type of ismarried = ", typeof(ismarried));
+// var ismarried = false;
+// console.log("value of ismarried = ",ismarried);
+// console.log("type of ismarried = ", typeof(ismarried));
 
-var x;
-console.log("type of x = ",typeof(x));
+// var x;
+// console.log("type of x = ",typeof(x));
+
+
+
 
 
 
---
+++ DataType/demo2.js
@@ -0,0 +1,49 @@
+// var age = 23;
+
+// console.log("Your age is: " + age);
+
+// var age = 45;
+
+// console.log("Your age is: " + age);
+
+
+// let email = "dev@gmail.com";
+// console.log("Email address = ",email);
+
+// email = "devnagariya@gmail.com";
+// console.log("Email address = ",email);
+
+
+// var a = 10;
+// console.log("outer value of a = ",a);
+
+// let b = 12.5;
+// console.log("outer value of b = ",b);
+
+// {
+//     console.log("inner value of a = ",a);
+//     console.log("inner value of b = ",b);
+    
+// }
+
+
+// {
+//     var a = 10;
+//     console.log("inner value of a = ",a);
+
+//     let b = 12.5;
+//     console.log("inner value of b = ",b);
+    
+    
+// }
+
+// console.log("outer value of a = ",a);
+// console.log("inner value of b = ",b);
+
+
+const pi = 3.14;
+console.log("value of pi = ",pi);
+
+pi = 3.15;
+console.log("value of pi = ",pi);
+
