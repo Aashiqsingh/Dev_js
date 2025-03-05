2662a0b49054a6d40171ca8d8f73a921e2d72281
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-05 14:24:48 +0530

    json

---
+++ json/json1.js
@@ -0,0 +1,21 @@
+var student = {
+    roll:101,
+    first_name:"dev",
+    last_name:"nagariya",
+    email:"dev@gmail.com",
+    gender:"male",
+    status:true
+}
+
+
+console.log(student);
+console.log(student.first_name);
+console.log(student.last_name);
+console.log(student.email);
+console.log(student.gender);
+console.log(student.status);
+
+
+
+
+
