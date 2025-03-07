05c7d4659b7896b38cacec55e664b8093c488e55
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-07 12:22:18 +0530

    json2

---
+++ json/json2.js
@@ -0,0 +1,46 @@
+var employee = {
+    empId:101,
+    name:"dev",
+    age:22,
+    department:"IT",
+    salary:50000,
+    email:"dev@gmail.com",
+    mobile:[7238727827, 873782729829],
+    isMarried:false,
+    address:{
+        street:"102 , 103 CG road",
+        area:"Navrangpura",
+        city:"Ahmedabad",
+        state:"Gujarat",
+        country:"India",
+        pincode:380001
+    }
+}
+
+console.log(employee);
+console.log(employee.name);
+console.log(employee.age);
+console.log(employee.department);
+console.log(employee.salary);
+console.log(employee.email);
+console.log(employee.mobile[0]);
+console.log(employee.mobile[1]);
+console.log(employee.isMarried);
+console.log(employee.address);
+console.log(employee.address.street);
+console.log(employee.address.city);
+console.log(employee.address.state);
+console.log(employee.address.country);
+console.log(employee.address.pincode);
+
+
+
+
+
+
+
+
+
+
+
+
