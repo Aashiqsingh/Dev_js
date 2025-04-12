4c1daa17b3ac6610c6f6419d7b7a54be9003f8fd
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-04-12 14:33:30 +0530

    function

---
+++ function/function1.js
@@ -0,0 +1,20 @@
+// There are four types of funcction 
+// 1. without return type without argumane
+// 2. withour return type with argument
+// 3. with return type without argument 
+// 4. with return type with argument 
+
+
+// 1. without return type without argumane
+
+
+function demo()
+{
+    console.log("demo function calledd......");
+    
+}
+
+
+
+
+demo()
