8193f7115f2ddc31f2f92b5630b93d0c429ba271
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-06-10 11:38:00 +0530

    form validation

--- form/form1.js
+++ form/form1.js
@@ -5,10 +5,54 @@ function submitHandler(event){
     let age = document.getElementById("age").value
     let email = document.getElementById("email").value
 
-    alert("Form submitted..")
+    let nameError = document.getElementById("nameError")
+    let ageError = document.getElementById("ageError")
+    let emailError = document.getElementById("emailError")
 
-    console.log("Name ----> ",name);
-    console.log("Age ----> ",age);
-    console.log("Email ----> ",email);
+    // alert("Form submitted..")
+
+    if(name === ""){
+        nameError.innerText = "Name is required"
+        nameError.style.color = "red"
+    }
+    else if(name.length < 4){
+        nameError.innerText = "Name should be atleast 4 characters"
+        nameError.style.color = "red"
+    }
+    else{
+        nameError.innerText = name 
+        nameError.style.color = "green"
+    }
+
+    if(age === ""){
+        ageError.innerText = "Age is required"
+        ageError.style.color = "red"
+    }
+    else if(age < 18){
+        ageError.innerText = "Age should be atleast 18 years"
+        ageError.style.color = "red"
+    }
+    else{
+        ageError.innerText = age 
+        ageError.style.color = "green"
+    
+    }
+
+    if(email === ""){
+        emailError.innerText = "Email is required"
+        emailError.style.color = "red"
+    }
+    else if(!email.includes("@")){
+        emailError.innerText = "Email should be not valid"
+        emailError.style.color = "red"
+    }
+    else{
+        emailError.innerText = email
+        emailError.style.color = "green"
+    }
+
+    // console.log("Name ----> ",name);
+    // console.log("Age ----> ",age);
+    // console.log("Email ----> ",email);
     
 }
